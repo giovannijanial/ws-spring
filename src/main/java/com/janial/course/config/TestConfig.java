ea9747fe7f19a7a9e7970cfff5063beeb33289d5
Giovanni Janial <giovannijanial@gmail.com>
2022-04-20 15:42:40 -0300

    Created OrderStatus Enums

--- src/main/java/com/janial/course/config/TestConfig.java
+++ src/main/java/com/janial/course/config/TestConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 import com.janial.course.entities.Order;
 import com.janial.course.entities.User;
+import com.janial.course.entities.enums.OrderStatus;
 import com.janial.course.repositories.OrderRepository;
 import com.janial.course.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
         User u3 = new User(null, "Carlos Magno", "carlos@gmail.com", "98444444", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
