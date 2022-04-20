79ed3b523c4d6c9c04a23d9ea09149f239b746df
Giovanni Janial <giovannijanial@gmail.com>
2022-04-20 16:01:38 -0300

    Created Caregory entity, repository, service and resource

--- src/main/java/com/janial/course/config/TestConfig.java
+++ src/main/java/com/janial/course/config/TestConfig.java
@@ -3,9 +3,11 @@ package com.janial.course.config;
 import java.time.Instant;
 import java.util.Arrays;
 
+import com.janial.course.entities.Category;
 import com.janial.course.entities.Order;
 import com.janial.course.entities.User;
 import com.janial.course.entities.enums.OrderStatus;
+import com.janial.course.repositories.CategoryRepository;
 import com.janial.course.repositories.OrderRepository;
 import com.janial.course.repositories.UserRepository;
 
@@ -23,6 +25,8 @@ public class TestConfig implements CommandLineRunner {
     private UserRepository userRepository;
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -34,7 +38,12 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 }
