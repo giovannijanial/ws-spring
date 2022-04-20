6890f008b79a0dd848c0ac73a8a060b9680a1258
Giovanni Janial <giovannijanial@gmail.com>
2022-04-20 16:30:07 -0300

    Created Product entity, repository, service and resouce

---
+++ src/main/java/com/janial/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.janial.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.janial.course.entities.Product;
+import com.janial.course.repositories.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll(){
+        return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
