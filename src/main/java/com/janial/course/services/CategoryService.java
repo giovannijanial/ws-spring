109786715c8ffe264874340b5bfef375ea8996e0
Giovanni Janial <giovannijanial@gmail.com>
2022-04-23 08:31:48 -0300

    Created Categories insert, update and delete

--- src/main/java/com/janial/course/services/CategoryService.java
+++ src/main/java/com/janial/course/services/CategoryService.java
@@ -3,10 +3,16 @@ package com.janial.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import com.janial.course.entities.Category;
 import com.janial.course.repositories.CategoryRepository;
+import com.janial.course.services.exceptions.DatabaseException;
+import com.janial.course.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,5 +28,36 @@ public class CategoryService {
     public Category findById(Long id){
         Optional<Category> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public Category insert(Category obj){
+        return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        try{
+            repository.deleteById(id);
+        }
+        catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }    
+    }
+
+    public Category update(Long id, Category obj){
+        try{
+            Category entity = repository.getById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }   
+    }
+
+    private void updateData(Category entity, Category obj) {
+        entity.setName(obj.getName());
     }
 }
